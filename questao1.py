3a82642fe735654a39c64b749403b335ae5d1bad
anacancado <142455517+anacancado@users.noreply.github.com>
2023-09-04 19:23:17 -0300

    Add files via upload

---
+++ questao1.py
@@ -0,0 +1,3 @@
+valorReal=float(input("Digite o valor em reais que voce deseja converter para dolares: "))
+valorDolar= valorReal/3.12
+print ("o valor convertido em dolar é igual a "+ str(valorDolar))
---
+++ questao2.py
@@ -0,0 +1,3 @@
+valorDolar=float(input("Digite o valor em dolares que voce deseja converter para reais: "))
+valorReal= valorDolar*3.12
+print ("o valor convertido em real é igual a "+ str(valorReal))
